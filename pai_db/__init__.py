396b5f260f33c874eb95792f9458aa14aa6f9c9b
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-01-03 20:56:57 -0800

    Initial commit of "pai_db" package.

---
+++ pai_db/__init__.py
@@ -0,0 +1,14 @@
+"""
+    pai_db
+    ~~~~~~
+
+    Persistent storage of defined resources/relations.
+
+    :copyright: (c) 2016 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
+
+__all__ = []
+
+
+__version__ = '0.0.1'
